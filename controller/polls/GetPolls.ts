bfed93dd661a55aa8ffae071dc8b26e6530fe2ce
Shubham-Tiwari-Anywhere <shubham.prem@anywhere.co>
2025-07-07 19:25:31 +0530

    updated sort order for polls

--- controller/polls/GetPolls.ts
+++ controller/polls/GetPolls.ts
@@ -3,7 +3,7 @@ import { Polls } from '../../schemas/Polls.js';
 
 export const getPolls = async (req: Request, res: Response) => {
   try {
-    const polls = await Polls.find().sort({ createdAt: 1 });
+    const polls = await Polls.find().sort({ createdAt: -1 });
     return res.status(200).json(polls);
   } catch (error) {
     console.error('Error fetching polls:', error);
